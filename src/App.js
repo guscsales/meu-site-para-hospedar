467a232e6fc25f4fe03ae963903b6ca22fd9713e
Gustavo Sales <gustavo@useorigin.com>
2021-02-15 10:28:16 -0300

    novo push para atualizar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Esse é meu site hospedado na Vercel, uhuul Gus Quem Fala!</h1>
+			<h2>Uma atualização nova no nosso website!</h2>
 		</div>
 	);
 }
